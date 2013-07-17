e17e15a02e12044988dc44d7caf1c86133f0acc3
Philipp Abraham <abrahamp@informatik.uni-luebeck.de>
2013-07-17 12:03:54 +0200

    [CoAP] fixed udp4radio

--- apps/pc_apps/coap_test/udp4radio.h
+++ apps/pc_apps/coap_test/udp4radio.h
@@ -41,8 +41,8 @@ namespace wiselib
 
 			IPv4Socket IPv4Ssock = IPv4Socket();
 			serv_addr = IPv4Ssock.operator int();
-			corr_index = 0;
-			correspondents.clear();
+			corr_index = 1; // adress 0 seems to make trouble so we start at 1
+			correspondents_.assign( NUM_CORRESPONDENTS, IPv4Socket() );
 		}
 
 		void set_socket( int socket )
@@ -71,8 +71,7 @@ namespace wiselib
 		
 		int send (node_id_t receiver_index, size_t len, block_data_t *data)
 		{
-			cout << "UDP4Radio::send>\n";
-			IPv4Socket receiver = correspondents.at( receiver_index );
+			IPv4Socket receiver = correspondents_.at( receiver_index );
 			sockaddr_in recv_sock;
 			recv_sock.sin_family = AF_INET;
 			recv_sock.sin_port = receiver.port();
@@ -80,15 +79,17 @@ namespace wiselib
 
 			if(enabled)
 			{
+#ifdef DEBUG_COAPRADIO_PC
 				cout << "UDP4Radio::send> sending " << len << " Bytes to "
-						<< ((recv_sock.sin_addr.s_addr & 0xff000000) >> 24)
-						<< "."
-						<< ((recv_sock.sin_addr.s_addr & 0x00ff0000) >> 16)
-						<< "."
-						<< ((recv_sock.sin_addr.s_addr & 0x0000ff00) >> 8)
-						<< "."
-						<< (recv_sock.sin_addr.s_addr & 0x000000ff)
-						<< "\n";
+										<< ((recv_sock.sin_addr.s_addr & 0xff000000) >> 24)
+										<< "."
+										<< ((recv_sock.sin_addr.s_addr & 0x00ff0000) >> 16)
+										<< "."
+										<< ((recv_sock.sin_addr.s_addr & 0x0000ff00) >> 8)
+										<< "."
+										<< (recv_sock.sin_addr.s_addr & 0x000000ff)
+										<< "\n";
+#endif
 				sendto( sock, data, len, 0, (struct sockaddr *) &recv_sock, sizeof( struct sockaddr_in ));
 				return OsModel::SUCCESS;
 			}
@@ -102,11 +103,16 @@ namespace wiselib
 
 		node_id_t add_correspondent( IPv4Socket corr )
 		{
-			node_id_t index = corr_index;
-			correspondents.at(index) = corr;
-			++corr_index;
-			if(corr_index == NUM_CORRESPONDENTS)
-				corr_index = 0;
+			node_id_t index = find_correspondent(corr);
+			if ( index == (NUM_CORRESPONDENTS+1) )
+			{
+				// not yet in list
+				index = corr_index;
+				correspondents_.at(index) = corr;
+				++corr_index;
+				if(corr_index == NUM_CORRESPONDENTS)
+					corr_index = 1;
+			}
 			return index;
 		}
 
@@ -116,7 +122,20 @@ namespace wiselib
 		node_id_t serv_addr;
 
 		size_t corr_index;
-		vector_static<OsModel, IPv4Socket, NUM_CORRESPONDENTS> correspondents;
+		vector_static<OsModel, IPv4Socket, NUM_CORRESPONDENTS> correspondents_;
+
+		size_t find_correspondent( IPv4Socket corr )
+		{
+			for ( size_t i = 1; i < correspondents_.size(); i++ )
+			{
+				IPv4Socket curr = correspondents_.at(i);
+				if ( curr == corr )
+				{
+					return i;
+				}
+			}
+			return NUM_CORRESPONDENTS +1;
+		}
 
 	};
 }
