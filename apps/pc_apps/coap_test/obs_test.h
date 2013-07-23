0ebb81d95802726adecbc22663daa4976863c9f5
Philipp Abraham <abrahamp@informatik.uni-luebeck.de>
2013-07-23 23:46:10 +0200

    [CoAP] retransmit notifications after max-age (closes #3)

--- apps/pc_apps/coap_test/obs_test.h
+++ apps/pc_apps/coap_test/obs_test.h
@@ -10,6 +10,8 @@
 
 #include "observable_service.h"
 
+#define OBS_TEST_INTERVAL 40000
+
 namespace wiselib
 {
 
@@ -26,20 +28,24 @@ public:
 	typedef ObsTest<Os, Radio, string_t> self_type;
 	typedef typename Radio::block_data_t block_data_t;
 	typedef typename Radio::ReceivedMessage coap_message_t;
+	typedef typename Radio::coap_packet_t coap_packet_t;
 	typedef struct ObservableService<Os_P, CoapRadio_P, String_T, uint>::message_data message_data;
 
 	ObsTest(string_t path, Radio& radio) :
 		ObservableService<Os_P, CoapRadio_P, String_T, uint>(path, 0, radio)
 	{
+		num_ = 100;
 		this->init();
 		this->template set_request_callback<self_type, &self_type::handle_request>(this);
-		timer_->template set_timer<self_type, &self_type::gen_number>(5000, this, 0);
+		this->set_max_age(20);
+		this->set_update_notification_confirmable(false);
+		timer_->template set_timer<self_type, &self_type::gen_number>(OBS_TEST_INTERVAL, this, 0);
 	}
 
 	void gen_number(void*) {
 		num_ = (++num_) % 1000;
 		this->set_status(num_);
-		timer_->template set_timer<self_type, &self_type::gen_number>(5000, this, 0);
+		timer_->template set_timer<self_type, &self_type::gen_number>(OBS_TEST_INTERVAL, this, 0);
 	}
 
 	void convert(uint value, message_data& payload) {
@@ -50,7 +56,24 @@ public:
 
 	void handle_request(coap_message_t& msg)
 	{
-		cout << "REQUEST\n";
+		coap_packet_t & packet = msg.message();
+
+		switch ( packet.code() ) {
+			case COAP_CODE_GET:
+				cout << "GET Request\n";
+				break;
+			case COAP_CODE_POST:
+				cout << "POST Request\n";
+				break;
+			case COAP_CODE_PUT:
+				cout << "PUT Request\n";
+				break;
+			case COAP_CODE_DELETE:
+				cout << "DELETE Request\n";
+				break;
+			default:
+				break;
+		}
 	}
 
 private:
